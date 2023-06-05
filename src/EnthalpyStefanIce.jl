5969f9e48f2e4d9927192748c7593661a7e9e8d1
jlk9 <josek97@utexas.edu>
2023-06-05 17:08:09 -0400

    Replaced CSR sparse matrix with tridiagonal and separated memory allocation and model run into distinct functions

--- src/EnthalpyStefanIce.jl
+++ src/EnthalpyStefanIce.jl
@@ -1,21 +1,21 @@
 module EnthalpyStefanIce
 
-using Plots, Roots, SparseArrays, SpecialFunctions
+using LinearAlgebra, Plots, Roots, SpecialFunctions
 
 # Write your package code here.
 
-#= Runs a mushy sea ice thermodynamic model using the Stefan number
+#= Builds a mushy sea ice thermodynamic model using the Stefan number
 
 Inputs:
-stefan  stefan number defined as Latent heat / sensible heat (L/cp*DeltaT), usually 10.0
+L           box size, usually 2
+N           number of cells, usually 2^8
+totaltime   total time usually 20
 
 =#
-function run_stefan_model(stefan)
+function build_stefan_model(L, N, totaltime)
 
     # quick finite volume
-    L       = 2                         # box size 
-    N       = 2^8
-    dy      = L/N;                      # number of cells and cell size
+    dy      = L/N;                      # cell size
     y       = range(start = dy/2, stop=L-(dy/2), step=dy) # location of cell centers
     y_edges = LinRange(0,L,N+1)         # location of cell edges
     k       = ones(length(y))           # thermal conductivity
@@ -23,9 +23,8 @@ function run_stefan_model(stefan)
     # TODO: can use collect on y, possible y_edges, i.e. y = collect(range(start = dy/2, stop=L-(dy/2), step=dy))
 
     # time information
-    totaltime = 20              # total time
-    dt        = (1/2.5)*(dy.^2) # timestep
-    Nt        = round(Int, totaltime/dt)    # total timesteps
+    dt = (1/2.5)*(dy.^2) # timestep
+    Nt = round(Int, totaltime/dt)    # total timesteps
 
     kup    = k[2:N]                              # thermal conductivity on i+1
     kdown  = k[1:(N-1)]                          # thermal conductivity on i-1
@@ -34,35 +33,55 @@ function run_stefan_model(stefan)
     TransN = (2/dy)*k[N]                         # transmissability at 1 and N
     Tup    = push!(deepcopy(Trans), TransN)                # i+1 transmissability to build matrix
     Tdown  = pushfirst!(deepcopy(Trans), Trans1)           # i-1 transmissability to build matrix
+    
+    M   = SymTridiagonal(-(Tup+Tdown), Trans)
+    M .*= dt/dy
+    
 
-    # derivative matrix
-    M_i = append!(collect(2:N), collect(1:N), collect(1:(N-1)))
-    M_j = append!(collect(1:(N-1)), collect(1:N), collect(2:N))
-    M_v = append!(deepcopy(Trans), deepcopy(-(Tup+Tdown)), deepcopy(Trans))
-    M   = sparse(M_i, M_j, M_v);
+    # boundary conditions pair
+    bcv_start = -1*Trans1 * (dt/dy)
+    bcv_end   = 0*TransN * (dt/dy)
 
-    # boundary conditions vector
-    bcv    = zeros(size(y))
-    bcv[1] = -1*Trans1
-    bcv[N] = 0*TransN
+    # solve using forward euler
+    enthalpy = ones(size(y));
 
-    # solve
-    initial_enthalpy = stefan .* ones(size(y)); # start with enthalpy = Stefan (all liquid)
-    # forward euler
-    enthalpy = initial_enthalpy
+    θ = zeros(size(y)) # temperature
     a = zeros(Nt)
-    t = zeros(Nt) # initialize
+    t = collect(dt:dt:(Nt*dt)) #time
+
+    return Nt, y, M, enthalpy, bcv_start, bcv_end, θ, a, t
+end
+
+#= Runs a mushy sea ice thermodynamic model using the Stefan number
+
+Inputs:
+Nt
+M
+enthalpy
+bcv_start
+bcv_end
+θ
+a
+stefan
+
+=#
+function run_stefan_model!(N, Nt, y, M, enthalpy, bcv_start, bcv_end, θ, a, stefan)
+
+    enthalpy .*= stefan # start with enthalpy = Stefan (all liquid)
     
     for i in 1:Nt
-        t[i]     = i*dt; # time 
-        theta    = min.(enthalpy,0); # temperature
-        enthalpy = enthalpy+(dt./dy)*(M*theta+bcv); # step equation
+        θ .= min.(enthalpy,0); # temperature
+
+        # step equation
+        enthalpy    += M * θ; # TODO: might be quicker to multiply with M.dv, M.ev
+        enthalpy[1] += bcv_start
+        enthalpy[N] += bcv_end
+        
         if minimum(enthalpy) <= 0
             a[i] = maximum(y[enthalpy .<= 0]); # determine location of ice-water interface
         end
+        
     end
-
-    return a, t, L
 end
 
 function plot_stefan_model(a, t, L, stefan)
